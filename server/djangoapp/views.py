f8f9cc969b1df6170799989922957b150adbeb2b
Saumya <saumya.srivastava20002@gmail.com>
2023-09-08 15:07:06 -0400

    Every

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-# from .restapis import related methods
+from .restapis import *
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
+from django.views.decorators.csrf import csrf_exempt
 import json
 
 # Get an instance of a logger
@@ -43,6 +44,7 @@ def login_request(request):
         if user is not None:
             # If user is valid, call login method to login current user
             login(request, user)
+            request.session['is_authenticated'] = True
             return redirect('djangoapp:index')
         else:
             # If not, return to login page again
@@ -55,6 +57,8 @@ def logout_request(request):
     print("Log out the user `{}`".format(request.user.username))
     # Logout user in the request
     logout(request)
+    if 'is_authenticated' in request.session:
+        del request.session['is_authenticated']
     # Redirect user back to course list view
     return redirect('djangoapp:index')
 
@@ -91,15 +95,76 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://saumyasriva2-3000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ','.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        context = {
+            "dealer_names": dealer_names,
+            }
+        return  HttpResponse(dealer_names)
+    return render(request, 'djangoapp/index.html', context)
 
 
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        url = "https://saumyasriva2-5000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"
+        # Get dealers from the URL
+        dealerships = get_dealer_reviews_from_cf(url, id=dealer_id)
+        # Concat all dealer's short name
+        reviews = ',\n'.join([f"{dealer.review}\n Sentiment: {dealer.sentiment}" for dealer in dealerships])
+        # Return a list of dealer short named
+        context = {
+            "reviews": reviews,
+            }
+        return HttpResponse(reviews)
+    return render(request, 'djangoapp/index.html', context)
 
-# Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+@csrf_exempt 
+def add_review(request, dealer_id):
+    if request.method == 'POST':
+
+        url = "https://saumyasriva2-5000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/post_review"
+        
+        name = request.POST.get('name')
+        dealership = request.POST.get('dealership')
+        review = request.POST.get('review')
+        purchase = request.POST.get('purchase')
+        purchase_date = request.POST.get('purchase_date')
+        car_make = request.POST.get('car_make')
+        car_model = request.POST.get('car_model')
+        car_year = request.POST.get('car_year')
+    
+        json_payload = {
+            'id': dealer_id, 
+            'name': name, 
+            'dealership': dealership, 
+            'review': review, 
+            'purchase': purchase, 
+            'purchase_date': purchase_date, 
+            'car_make': car_make, 
+            'car_model': car_model, 
+            'car_year': car_year
+        }
+
+        try:
+            # Set the Content-Type header to 'application/json'
+            headers = {'Content-Type': 'application/json'}
+
+            # Send a POST request with JSON data
+            response = requests.post(url, json_payload=json_payload, headers=headers)
+
+            if response.status_code == 200:
+                # Assuming the external API returns a JSON response
+                response_data = response.json()
+                return JsonResponse(response_data)
+            else:
+                return JsonResponse({'error': 'Failed to post review to the external API'})
+        except Exception as e:
+            # Handle any exceptions that may occur during the request
+            return JsonResponse({'error': f'An error occurred: {str(e)}'})
+
+    return JsonResponse({'error': 'Only POST requests are allowed'})
